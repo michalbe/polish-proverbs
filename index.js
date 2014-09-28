a4888a07b48f4b613a6ff88c0e28b6e5ac3460aa
Michał Budzyński <michal@virtualdesign.pl>
2014-09-28 01:01:00 +0100

    [POLISH-PROVERBS] - :whale: get one proverb

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 'use strict';
 
-//var jsdom = require('jsdom');
+var jsdom = require('jsdom');
 var fs = require('fs');
 
-var PROVERB_FILE = __dirname + '/proverbs.txt';
-console.log(PROVERB_FILE);
+var PP = (function() {
+  var PROVERB_FILE = __dirname + '/proverbs.txt';
+  var host = 'http://pl.wiktionary.org';
+  var urls = [
+    '/wiki/Kategoria:Polskie_przysłowia',
+    '/w/index.php?title=Kategoria:Polskie_przys%C5%82owia&pagefrom=' +
+      'kropla+dr%C4%85%C5%BCy+ska%C5%82%C4%99#mw-pages',
+    '/w/index.php?title=Kategoria:Polskie_przysłowia&pagefrom=' +
+      'w+święto+Trzech+Króli+człek+się+w+kożuch+tuli#mw-pages'
+  ];
 
-if (fs.existsSync(PROVERB_FILE)) {
-    // Do something
-}
+  var getProverbs = function(cb) {
+    jsdom.env(
+      host+urls[0],
+      [],
+      function (errors, window) {
+        if (errors) {
+          cb(errors);
+          return;
+        }
+        cb(null, window.document.querySelectorAll('li > a')[3]);
+      }
+    );
+  }
+
+  if (fs.existsSync(PROVERB_FILE)) {
+    console.log('we have list');
+  } else {
+    getProverbs(function(err, proverb){
+      if (!err){
+        console.log(proverb.innerHTML);
+      }
+    });
+  }
+});
+
+PP();
+module.exports = PP;
